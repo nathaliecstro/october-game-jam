0a91449153f9580426d8e9e8ad5dd634a6be679a
Miguel A Maldonado Maldonado <miguel.maldonado10@upr.edu>
2025-10-07 23:25:03 -0400

    Implemented music

--- october-game-jam/scripts/question_1_scene.gd
+++ october-game-jam/scripts/question_1_scene.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	AudioPlayer.play_music_level()
 	$transition/AnimationPlayer.play("fade_out")
 	await get_tree().create_timer(1.5).timeout
 	
